d446b0d6848f77d1881c4b37227a0716b179cf05
Ticknor, Samantha <TicknorS@CarnegieMuseums.Org>
2017-03-03 12:32:05 -0500

    Fixed scheduling notifications on android 5.0 and below

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -35,18 +35,18 @@ import static com.dieam.reactnativepushnotification.modules.RNPushNotificationAt
 public class RNPushNotificationHelper {
     public static final String PREFERENCES_KEY = "rn_push_notification";
     private static final long DEFAULT_VIBRATION = 300L;
-
+    
     private Context context;
     private final SharedPreferences scheduledNotificationsPersistence;
     private static final int ONE_MINUTE = 60 * 1000;
     private static final long ONE_HOUR = 60 * ONE_MINUTE;
     private static final long ONE_DAY = 24 * ONE_HOUR;
-
+    
     public RNPushNotificationHelper(Application context) {
         this.context = context;
         this.scheduledNotificationsPersistence = context.getSharedPreferences(RNPushNotificationHelper.PREFERENCES_KEY, Context.MODE_PRIVATE);
     }
-
+    
     public Class getMainActivityClass() {
         String packageName = context.getPackageName();
         Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
@@ -58,79 +58,76 @@ public class RNPushNotificationHelper {
             return null;
         }
     }
-
+    
     private AlarmManager getAlarmManager() {
         return (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
     }
-
+    
     private PendingIntent toScheduleNotificationIntent(Bundle bundle) {
         int notificationID = Integer.parseInt(bundle.getString("id"));
-
+        
         Intent notificationIntent = new Intent(context, RNPushNotificationPublisher.class);
         notificationIntent.putExtra(RNPushNotificationPublisher.NOTIFICATION_ID, notificationID);
         notificationIntent.putExtras(bundle);
-
+        
         return PendingIntent.getBroadcast(context, notificationID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
-
+    
     public void sendNotificationScheduled(Bundle bundle) {
         Class intentClass = getMainActivityClass();
         if (intentClass == null) {
             Log.e(LOG_TAG, "No activity class found for the scheduled notification");
             return;
         }
-
+        
         if (bundle.getString("message") == null) {
             Log.e(LOG_TAG, "No message specified for the scheduled notification");
             return;
         }
-
+        
         if (bundle.getString("id") == null) {
             Log.e(LOG_TAG, "No notification ID specified for the scheduled notification");
             return;
         }
-
+        
         double fireDate = bundle.getDouble("fireDate");
         if (fireDate == 0) {
             Log.e(LOG_TAG, "No date specified for the scheduled notification");
             return;
         }
-
+        
         RNPushNotificationAttributes notificationAttributes = new RNPushNotificationAttributes(bundle);
         String id = notificationAttributes.getId();
-
+        
         Log.d(LOG_TAG, "Storing push notification with id " + id);
-
+        
         SharedPreferences.Editor editor = scheduledNotificationsPersistence.edit();
         editor.putString(id, notificationAttributes.toJson().toString());
         commit(editor);
-
+        
         boolean isSaved = scheduledNotificationsPersistence.contains(id);
         if (!isSaved) {
             Log.e(LOG_TAG, "Failed to save " + id);
         }
-
+        
         sendNotificationScheduledCore(bundle);
     }
-
+    
     public void sendNotificationScheduledCore(Bundle bundle) {
         long fireDate = (long) bundle.getDouble("fireDate");
-
+        
         // If the fireDate is in past, this will fire immediately and show the
         // notification to the user
         PendingIntent pendingIntent = toScheduleNotificationIntent(bundle);
-
-        Log.d(LOG_TAG, String.format("Setting a notification with id %s at time %s",
-                bundle.getString("id"), Long.toString(fireDate)));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
             getAlarmManager().setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        } else if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
             getAlarmManager().setExact(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
         } else {
             getAlarmManager().set(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
         }
     }
-
+    
     public void sendToNotificationCentre(Bundle bundle) {
         try {
             Class intentClass = getMainActivityClass();
@@ -138,35 +135,35 @@ public class RNPushNotificationHelper {
                 Log.e(LOG_TAG, "No activity class found for the notification");
                 return;
             }
-
+            
             if (bundle.getString("message") == null) {
                 // this happens when a 'data' notification is received - we do not synthesize a local notification in this case
                 Log.d(LOG_TAG, "Cannot send to notification centre because there is no 'message' field in: " + bundle);
                 return;
             }
-
+            
             String notificationIdString = bundle.getString("id");
             if (notificationIdString == null) {
                 Log.e(LOG_TAG, "No notification ID specified for the notification");
                 return;
             }
-
+            
             Resources res = context.getResources();
             String packageName = context.getPackageName();
-
+            
             String title = bundle.getString("title");
             if (title == null) {
                 ApplicationInfo appInfo = context.getApplicationInfo();
                 title = context.getPackageManager().getApplicationLabel(appInfo).toString();
             }
-
+            
             NotificationCompat.Builder notification = new NotificationCompat.Builder(context)
-                    .setContentTitle(title)
-                    .setTicker(bundle.getString("ticker"))
-                    .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-                    .setPriority(NotificationCompat.PRIORITY_MAX)
-                    .setAutoCancel(bundle.getBoolean("autoCancel", true));
-
+            .setContentTitle(title)
+            .setTicker(bundle.getString("ticker"))
+            .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(bundle.getBoolean("autoCancel", true));
+            
             String group = bundle.getString("group");
             if (group != null) {
                 notification.setGroup(group);
@@ -176,77 +173,77 @@ public class RNPushNotificationHelper {
             if (alarmUUID != null) {
                 bundle.putDouble("alarmUUID", alarmUUID);
             }
-
+            
             notification.setContentText(bundle.getString("message"));
-
+            
             String largeIcon = bundle.getString("largeIcon");
-
+            
             String subText = bundle.getString("subText");
-
+            
             if (subText != null) {
                 notification.setSubText(subText);
             }
-
+            
             String numberString = bundle.getString("number");
             if (numberString != null) {
                 notification.setNumber(Integer.parseInt(numberString));
             }
-
+            
             int smallIconResId;
             int largeIconResId;
-
+            
             String smallIcon = bundle.getString("smallIcon");
-
+            
             if (smallIcon != null) {
                 smallIconResId = res.getIdentifier(smallIcon, "mipmap", packageName);
             } else {
                 smallIconResId = res.getIdentifier("ic_notification", "mipmap", packageName);
             }
-
+            
             if (smallIconResId == 0) {
                 smallIconResId = res.getIdentifier("ic_launcher", "mipmap", packageName);
-
+                
                 if (smallIconResId == 0) {
                     smallIconResId = android.R.drawable.ic_dialog_info;
                 }
             }
-
+            
             if (largeIcon != null) {
                 largeIconResId = res.getIdentifier(largeIcon, "mipmap", packageName);
             } else {
                 largeIconResId = res.getIdentifier("ic_launcher", "mipmap", packageName);
             }
-
+            
             Bitmap largeIconBitmap = BitmapFactory.decodeResource(res, largeIconResId);
-
+            
             if (largeIconResId != 0 && (largeIcon != null || Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)) {
                 notification.setLargeIcon(largeIconBitmap);
             }
-
+            
             notification.setSmallIcon(smallIconResId);
             String bigText = bundle.getString("bigText");
-
+            
             if (bigText == null) {
                 bigText = bundle.getString("message");
             }
-
+            
             notification.setStyle(new NotificationCompat.BigTextStyle().bigText(bigText));
-
+            
             Intent intent = new Intent(context, intentClass);
             intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
             bundle.putBoolean("userInteraction", true);
             intent.putExtra("notification", bundle);
-
+            
             if (!bundle.containsKey("playSound") || bundle.getBoolean("playSound")) {
                 Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                 String soundName = bundle.getString("soundName");
                 if (soundName != null) {
                     if (!"default".equalsIgnoreCase(soundName)) {
-
+                        
                         // sound name can be full filename, or just the resource name.
                         // So the strings 'my_sound.mp3' AND 'my_sound' are accepted
                         // The reason is to make the iOS and android javascript interfaces compatible
-
+                        
                         int resId;
                         if (context.getResources().getIdentifier(soundName, "raw", context.getPackageName()) != 0) {
                             resId = context.getResources().getIdentifier(soundName, "raw", context.getPackageName());
@@ -254,53 +251,53 @@ public class RNPushNotificationHelper {
                             soundName = soundName.substring(0, soundName.lastIndexOf('.'));
                             resId = context.getResources().getIdentifier(soundName, "raw", context.getPackageName());
                         }
-
+                        
                         soundUri = Uri.parse("android.resource://" + context.getPackageName() + "/" + resId);
                     }
                 }
                 notification.setSound(soundUri, AudioManager.STREAM_ALARM);
             }
-
+            
             if (bundle.containsKey("ongoing") || bundle.getBoolean("ongoing")) {
                 notification.setOngoing(bundle.getBoolean("ongoing"));
             }
-
+            
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 notification.setCategory(NotificationCompat.CATEGORY_ALARM);
-
+                
                 String color = bundle.getString("color");
                 if (color != null) {
                     notification.setColor(Color.parseColor(color));
                 }
             }
-
+            
             int notificationID = Integer.parseInt(notificationIdString);
-
+            
             PendingIntent pendingIntent = PendingIntent.getActivity(context, notificationID, intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-
+                                                                    PendingIntent.FLAG_UPDATE_CURRENT);
+            
             NotificationManager notificationManager = notificationManager();
-
+            
             notification.setContentIntent(pendingIntent);
-
+            
             if (!bundle.containsKey("vibrate") || bundle.getBoolean("vibrate")) {
                 long vibration = bundle.containsKey("vibration") ? (long) bundle.getDouble("vibration") : DEFAULT_VIBRATION;
                 if (vibration == 0)
                     vibration = DEFAULT_VIBRATION;
                 notification.setVibrate(new long[]{0, vibration});
             }
-
+            
             JSONArray actionsArray = null;
             try {
                 actionsArray = bundle.getString("actions") != null ? new JSONArray(bundle.getString("actions")) : null;
             } catch (JSONException e) {
                 Log.e(LOG_TAG, "Exception while converting actions to JSON object.", e);
             }
-
+            
             if (actionsArray != null) {
                 // No icon for now. The icon value of 0 shows no icon.
                 int icon = 0;
-
+                
                 // Add button for each actions.
                 for (int i = 0; i < actionsArray.length(); i++) {
                     String action;
@@ -310,7 +307,7 @@ public class RNPushNotificationHelper {
                         Log.e(LOG_TAG, "Exception while getting action from actionsArray.", e);
                         continue;
                     }
-
+                    
                     Intent actionIntent = new Intent(context, intentClass);
                     actionIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                     actionIntent.setAction(context.getPackageName() + "." + action);
@@ -318,11 +315,11 @@ public class RNPushNotificationHelper {
                     bundle.putString("action", action);
                     actionIntent.putExtra("notification", bundle);
                     PendingIntent pendingActionIntent = PendingIntent.getActivity(context, notificationID, actionIntent,
-                            PendingIntent.FLAG_UPDATE_CURRENT);
+                                                                                  PendingIntent.FLAG_UPDATE_CURRENT);
                     notification.addAction(icon, action, pendingActionIntent);
                 }
             }
-
+            
             // Remove the notification from the shared preferences once it has been shown
             // to avoid showing the notification again when the phone is rebooted. If the
             // notification is not removed, then every time the phone is rebooted, we will
@@ -336,17 +333,17 @@ public class RNPushNotificationHelper {
                 editor.remove(notificationIdString);
                 commit(editor);
             }
-
+            
             Notification info = notification.build();
             info.defaults |= Notification.DEFAULT_LIGHTS;
-
+            
             if (bundle.containsKey("tag")) {
                 String tag = bundle.getString("tag");
                 notificationManager.notify(tag, notificationID, info);
             } else {
                 notificationManager.notify(notificationID, info);
             }
-
+            
             // Can't use setRepeating for recurring notifications because setRepeating
             // is inexact by default starting API 19 and the notifications are not fired
             // at the exact time. During testing, it was found that notifications could
@@ -356,30 +353,30 @@ public class RNPushNotificationHelper {
             Log.e(LOG_TAG, "failed to send push notification", e);
         }
     }
-
+    
     private void scheduleNextNotificationIfRepeating(Bundle bundle) {
         String repeatType = bundle.getString("repeatType");
         long repeatTime = (long) bundle.getDouble("repeatTime");
-
+        
         if (repeatType != null) {
             long fireDate = (long) bundle.getDouble("fireDate");
-
+            
             boolean validRepeatType = Arrays.asList("time", "week", "day", "hour", "minute").contains(repeatType);
-
+            
             // Sanity checks
             if (!validRepeatType) {
                 Log.w(LOG_TAG, String.format("Invalid repeatType specified as %s", repeatType));
                 return;
             }
-
+            
             if ("time".equals(repeatType) && repeatTime <= 0) {
                 Log.w(LOG_TAG, "repeatType specified as time but no repeatTime " +
-                        "has been mentioned");
+                      "has been mentioned");
                 return;
             }
-
+            
             long newFireDate = 0;
-
+            
             switch (repeatType) {
                 case "time":
                     newFireDate = fireDate + repeatTime;
@@ -397,32 +394,32 @@ public class RNPushNotificationHelper {
                     newFireDate = fireDate + ONE_MINUTE;
                     break;
             }
-
+            
             // Sanity check, should never happen
             if (newFireDate != 0) {
                 Log.d(LOG_TAG, String.format("Repeating notification with id %s at time %s",
-                        bundle.getString("id"), Long.toString(newFireDate)));
+                                             bundle.getString("id"), Long.toString(newFireDate)));
                 bundle.putDouble("fireDate", newFireDate);
                 this.sendNotificationScheduled(bundle);
             }
         }
     }
-
+    
     public void clearNotifications() {
         Log.i(LOG_TAG, "Clearing alerts from the notification centre");
-
+        
         NotificationManager notificationManager = notificationManager();
         notificationManager.cancelAll();
     }
-
+    
     public void cancelAllScheduledNotifications() {
         Log.i(LOG_TAG, "Cancelling all notifications");
-
+        
         for (String id : scheduledNotificationsPersistence.getAll().keySet()) {
             cancelScheduledNotification(id);
         }
     }
-
+    
     public void cancelScheduledNotification(ReadableMap userInfo) {
         for (String id : scheduledNotificationsPersistence.getAll().keySet()) {
             try {
@@ -437,15 +434,15 @@ public class RNPushNotificationHelper {
             }
         }
     }
-
+    
     private void cancelScheduledNotification(String notificationIDString) {
         Log.i(LOG_TAG, "Cancelling notification: " + notificationIDString);
-
+        
         // remove it from the alarm manger schedule
         Bundle b = new Bundle();
         b.putString("id", notificationIDString);
         getAlarmManager().cancel(toScheduleNotificationIntent(b));
-
+        
         if (scheduledNotificationsPersistence.contains(notificationIDString)) {
             // remove it from local storage
             SharedPreferences.Editor editor = scheduledNotificationsPersistence.edit();
@@ -454,17 +451,17 @@ public class RNPushNotificationHelper {
         } else {
             Log.w(LOG_TAG, "Unable to find notification " + notificationIDString);
         }
-
+        
         // removed it from the notification center
         NotificationManager notificationManager = notificationManager();
-
+        
         notificationManager.cancel(Integer.parseInt(notificationIDString));
     }
-
+    
     private NotificationManager notificationManager() {
         return (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
-
+    
     private static void commit(SharedPreferences.Editor editor) {
         if (Build.VERSION.SDK_INT < 9) {
             editor.commit();
